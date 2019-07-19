64c7fa6366313ef49e1cbb48b548c1d186d5ffd3
Ryan Graff <ryan.graff@bedrockdata.com>
2019-07-19 15:38:52 -0400

    test: Add failing tests for the Google provider.

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,10 @@
 import AWSConfigLoaderInternal from './Providers/AWS/ConfigLoader';
 import AWSInternal from './Providers/AWS';
+import GoogleInternal from './Providers/Google';
 import ServiceResponseInternal from './Utils/ServiceResponse';
 
 export const ServiceResponse = ServiceResponseInternal;
 
 export const AWSConfigLoader = AWSConfigLoaderInternal;
 export const AWS = AWSInternal;
+export const Google = GoogleInternal;
--- src/index.spec.jsx
+++ src/index.spec.jsx
@@ -1,7 +1,19 @@
 import expect from 'expect.js';
-import {AWS} from './index.jsx';
+import {AWS, Google} from './index.jsx';
 import MockAPIGatewayEvent from '../Mock Data/AWS/Mock API Gateway Event';
+import MockGCFRequestContext from '../Mock Data/Google/Mock GCF Request Context';
 import {DEP_NAMES} from './Providers/Constants';
+
+const MockResponseInstance = {
+  set: () => {
+  },
+  status: () => ({
+    send: () => {
+    }
+  }),
+  send: () => {
+  }
+};
 
 module.exports = {
   AWS: {
@@ -171,6 +183,195 @@ module.exports = {
         dependencyResolutionTimeoutMS: 1000
       });
       const result = await cloudFunction(MockAPIGatewayEvent);
+      const {
+        statusCode,
+        body: responseBody = '{}'
+      } = result || {};
+      const {
+        event,
+        context,
+        identity
+      } = JSON.parse(responseBody) || {};
+
+      expect(statusCode).to.be(200);
+
+      expect(event).to.be.an(Object);
+      expect(event.test).to.be('TEST_EVENT');
+
+      expect(context).to.be.an(Object);
+      expect(context.test).to.be('TEST_CONTEXT');
+
+      expect(identity).to.be.an(Object);
+      expect(identity.test).to.be('TEST_IDENTITY');
+    }
+  },
+  Google: {
+    'should return a response': async () => {
+      const cloudFunction = Google({
+        incarnateConfig: {
+          subMap: {
+            package: {
+              subMap: {
+                service: {
+                  factory: () => {
+                    return {
+                      methodName: a1 => a1
+                    };
+                  }
+                }
+              }
+            }
+          }
+        },
+        allowedPaths: [
+          '/package/service/method-name'
+        ],
+        allowedOrigin: 'http://example.com'
+      });
+      const {
+        body: responseBody
+      } = await cloudFunction(MockGCFRequestContext, MockResponseInstance);
+
+      expect(responseBody).to.be.a('string');
+
+      const parsedResponseBody = JSON.parse(responseBody);
+
+      expect(parsedResponseBody).to.be.a('string');
+      expect(parsedResponseBody).to.equal('Arguments in JSON');
+    },
+    'should return asynchronous dependency errors': async () => {
+      const errorMessage = 'X_FAILED';
+      const cloudFunction = Google({
+        incarnateConfig: {
+          subMap: {
+            config: {
+              subMap: {
+                x: {
+                  factory: async () => {
+                    throw new Error(errorMessage);
+                  }
+                }
+              }
+            },
+            package: {
+              shared: {
+                config: 'config'
+              },
+              subMap: {
+                service: {
+                  dependencies: {
+                    x: 'config/x'
+                  },
+                  strict: true,
+                  factory: () => {
+                    return {
+                      methodName: a1 => a1
+                    };
+                  }
+                }
+              }
+            }
+          }
+        },
+        allowedPaths: [
+          '/package/service/method-name'
+        ],
+        allowedOrigin: 'http://example.com'
+      });
+      const result = await cloudFunction(MockGCFRequestContext, MockResponseInstance);
+      const {
+        statusCode,
+        body: responseBody = 'undefined'
+      } = result || {};
+      const {
+        source: {
+          message: sourceMessage
+        } = {}
+      } = JSON.parse(responseBody) || {};
+
+      expect(statusCode).to.be(500);
+      expect(sourceMessage).to.be(errorMessage);
+    },
+    'should handle missing strict dependencies gracefully': async () => {
+      const cloudFunction = Google({
+        incarnateConfig: {
+          subMap: {
+            config: {
+              subMap: {
+                x: {
+                  factory: () => undefined
+                }
+              }
+            },
+            package: {
+              shared: {
+                config: 'config'
+              },
+              subMap: {
+                service: {
+                  dependencies: {
+                    x: 'config/x'
+                  },
+                  strict: true,
+                  factory: () => {
+                    return {
+                      methodName: a1 => a1
+                    };
+                  }
+                }
+              }
+            }
+          }
+        },
+        allowedPaths: [
+          '/package/service/method-name'
+        ],
+        allowedOrigin: 'http://example.com',
+        // IMPORTANT: Add a reasonable timeout.
+        dependencyResolutionTimeoutMS: 1000
+      });
+      const result = await cloudFunction(MockGCFRequestContext, MockResponseInstance);
+      const {
+        statusCode,
+        body: responseBody = '{}'
+      } = result || {};
+      const {message} = JSON.parse(responseBody) || {};
+
+      expect(statusCode).to.be(500);
+      expect(message).to.be('RESOLUTION_TIMEOUT');
+    },
+    'should supply request specific built-in dependencies': async () => {
+      const getDepPath = p => `${DEP_NAMES.INPUT}/${p}`;
+      const cloudFunction = Google({
+        incarnateConfig: {
+          subMap: {
+            package: {
+              shared: {
+                [DEP_NAMES.INPUT]: DEP_NAMES.INPUT
+              },
+              subMap: {
+                service: {
+                  dependencies: {
+                    event: getDepPath(DEP_NAMES.EVENT),
+                    context: getDepPath(DEP_NAMES.CONTEXT),
+                    identity: getDepPath(DEP_NAMES.IDENTITY)
+                  },
+                  factory: d => ({
+                    methodName: () => d
+                  })
+                }
+              }
+            }
+          }
+        },
+        allowedPaths: [
+          '/package/service/method-name'
+        ],
+        allowedOrigin: 'http://example.com',
+        // IMPORTANT: Add a reasonable timeout.
+        dependencyResolutionTimeoutMS: 1000
+      });
+      const result = await cloudFunction(MockGCFRequestContext, MockResponseInstance);
       const {
         statusCode,
         body: responseBody = '{}'
