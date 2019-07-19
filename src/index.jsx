a84e4024d933533927d5055163aea2ae8aa19ad8
Ryan Graff <ryan.graff@bedrockdata.com>
2019-07-19 15:41:28 -0400

    feat: (index) Add DEP_NAMES to exports.

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,9 @@ import AWSConfigLoaderInternal from './Providers/AWS/ConfigLoader';
 import AWSInternal from './Providers/AWS';
 import GoogleInternal from './Providers/Google';
 import ServiceResponseInternal from './Utils/ServiceResponse';
+import {DEP_NAMES as DEP_NAMES_INTERNAL} from './Providers/Constants';
+
+export const DEP_NAMES = DEP_NAMES_INTERNAL;
 
 export const ServiceResponse = ServiceResponseInternal;
 
